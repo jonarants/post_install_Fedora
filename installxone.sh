1a101ce59c7939b4271b678b63b9e63a4995ee79
jonarants <jonathan.vazquezd@outlook.es>
2026-06-28 02:41:33 -0600

    Add commands to install and configure xone

--- installxone.sh
+++ installxone.sh
@@ -3,6 +3,7 @@
 echo "Installing xbox dongle with XONE"
 sudo dnf install dkms make kernel-devel-$(uname -r) kernel-headers git cabextract
 git clone https://github.com/medusalix/xone
+cd xone
 sudo sh install.sh --release
 sudo xone-get-firmware.sh
 sudo rm -rf xone
